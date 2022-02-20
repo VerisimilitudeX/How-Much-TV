b4405827e3c4cfcd53a9404be686f63e7fb057d3
Verisimilitude <acharyapiyush1@outlook.com>
2022-02-19 17:40:53 -0800

    Update How_Much_TV.py

--- How_Much_TV.py
+++ How_Much_TV.py
@@ -1,11 +1,8 @@
 age = input("How old are you? ")
-
 hours = input("How may hours do you spend each day watching TV? ")
-
 total = float(hours) * int(age) * 365
 
 print("Hours spent watching videos: " + str(total))
 
 days = int(total/24)
-
 print("That's " + str(days) + " days!")
